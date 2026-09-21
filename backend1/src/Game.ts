41fd28e32b61a58a617283abe141ced0c59cbc3c
alisha <codealisha99@gmail.com>
2026-09-21 23:52:06 +0530

    debugging

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,5 +1,6 @@
 import { WebSocket } from "ws";
 import { Chess } from 'chess.js';
+import { INIT_GAME, MOVE } from "./message";
 
 export class Game {
     public player1: WebSocket;
@@ -14,64 +15,68 @@ export class Game {
         this.player2 = player2;
         this.board = new Chess();
         this.startTime = new Date();
+
+
+
         this.player1.send(JSON.stringify({
-            type: "init_game",
+            type: INIT_GAME,
             payload: {
                 color: "white",
-                opponent: "black"
+                
             }
         }))
         this.player2.send(JSON.stringify({
-            type: "init_game",
+            type: INIT_GAME,
             payload: {
-                color: "white",
-                opponent: "black"
+                color: "black",
+               
             }
         }))
 
     }
-
-    makeMove(player: WebSocket, move: {
-        from: string;
-        to: string;
-    }) {
+    //method-1
+    makeMove(player: WebSocket, move: {from: string;to: string;}) 
+    {
+        // 1validating the move 
         if(this.board.move.length % 2 === 0 && player !== this.player1) { return; }
         if(this.board.move.length % 2 === 1 && player !== this.player2) { return; }
 
 
-
+         //2) making the move on the board
          try{
             this.board.move(move);
          }
          catch(e) {
+            console.log("Invalid move:", e);
             return;
          }
 
-
+           //3) game over check
          if(this.board.isGameOver()) {
             //this will notify player1 that the game is over
-           this.player1.emit(JSON.stringify({
+           this.player1.send(JSON.stringify({
                type: "game_over",
                payload: {
                 winner: this.board.turn() === 'w' ? 'black' : 'white',
                }
              }))
             //this will notify player2 that the game is over 
-              this.player2.emit(JSON.stringify({
+              this.player2.send(JSON.stringify({
                  type: "game_over",
                  payload: {
                       winner: this.board.turn() === 'w' ? 'black' : 'white',
                  }
          }))
-
+        
+         //4) sending the move to the other player
          if (this.board.moves.length % 2 === 0) {
             this.player2.emit(JSON.stringify({
-                type: "move",
+                type: MOVE,
                 payload: move
             }))
          } else {
             this.player1.emit(JSON.stringify({
-                type: "move",
+                type: MOVE,
                 payload: move
             }))
          }
